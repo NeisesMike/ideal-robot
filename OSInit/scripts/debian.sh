f590e85ab7e4fe2990cf6ba1d88d5d59b5d59dd5
Michael Neises <neisesmichael@gmail.com>
2018-04-26 02:58:40 +0000

    add flourishes to debian.sh

--- OSInit/scripts/debian.sh
+++ OSInit/scripts/debian.sh
@@ -1,16 +1,19 @@
 #== Michael Neises
 #== 25/4/18
-#== set my system up so that it isn't totally ugly
+#== set the system up so that it isn't totally ugly
+
+#get zsh and set it as the default shell
+##we do this here so that all password requests are grouped at the beginning
+sudo apt-get install zsh
+chsh -s $(which zsh)
 
 #do some updates
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install cmus git i3 vim zsh
+sudo apt-get install cmus git i3 vim
+sudo apt-get autoclean; sudo apt-get autoremove
 
 #get oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-#set zsh as the default shell
-chsh -s $(which zsh)
 
 #create a projects directory
 mkdir ~/Documents/projects
@@ -26,7 +29,14 @@ cd ${ideal}/OSInit/configFiles && cp -r .gitconfig .i3 .vimrc .zshrc ~/
 #copy music
 cd ${ideal}/OSInit/tunes && cp -r * ~/Music/
 
-#
+#output some congratulatory message
+
+echo "              __ "
+echo "             / _)"        
+echo "      .-^^^-/ /  "       
+echo "   __/       /   "          
+echo "  <__.|_|-|_|    "         
+echo "                 "
 echo "===================================================="
 echo "Please verify the configuration options in ~/.zshrc!"
 echo "===================================================="
