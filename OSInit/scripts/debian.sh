dad84c3883cfd9cf9b2123e440ab38b6eeb5e9cb
michael neises <m811n155@ku.edu>
2018-04-26 00:23:09 +0000

    populate debian.sh

--- OSInit/scripts/debian.sh
+++ OSInit/scripts/debian.sh
@@ -3,17 +3,25 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install cmus git i3 vim zsh
 
 #get oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #set zsh as the default shell
 chsh -s $(which zsh)
 
 #create a projects directory
+mkdir ~/Documents/projects && cd ~/Documents/projects
 
 #clone ideal-robot into the projects directory
+git clone https://github.com/neisesMike/ideal-robot.git
+ideal = '~/Documents/projects/ideal'
 
 #copy config files
+cd ${ideal}'/OSInit/configFiles'
+cp -r .gitconfig .i3 .vimrc .zshrc ~/
 
 #copy music
+cd ${ideal}'/OSInit/tunes'
+cp -r * ~/Music/
 
 #get summa dat yung reboot action
 #sudo reboot
