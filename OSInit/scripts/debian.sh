464c316dd3a8b1d2f156ea34ec4c50291d72b1e6
michael neises <m811n155@ku.edu>
2018-04-26 00:15:58 +0000

    move tunes and begin deb script

---
+++ OSInit/scripts/debian.sh
@@ -0,0 +1,19 @@
+#do some updates
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install cmus git i3 vim zsh
+
+#get oh-my-zsh
+
+#set zsh as the default shell
+chsh -s $(which zsh)
+
+#create a projects directory
+
+#clone ideal-robot into the projects directory
+
+#copy config files
+
+#copy music
+
+#get summa dat yung reboot action
+#sudo reboot
