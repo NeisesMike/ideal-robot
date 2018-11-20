e486f62cfc638c3f35f82ac19cba2f64d0d688a3
michael <neisesmichael@gmail.com>
2018-11-20 19:03:00 +0000

    add gpio signal killer, is bad practice, sorry

--- rpiproj/killgpio.py
+++ rpiproj/killgpio.py
@@ -1,15 +1,28 @@
 # mike
-# 18 November 2018
+# 20 november 18
 
-import RPi.GPIO as GPIO
-import piLibe.relay
 import piLibe.hygrometer
+import piLibe.relay
 import piLibe.solenoidValve
+import RPi.GPIO as GPIO
 
-GPIO.setmode( GPIO.BCM )
+gpios = [23,24,25,16,17,27,22,26]
 
-piLibe.hygrometer.initHygrometer( 23 )
-piLibe.relay.initRelay( 24 )
-piLibe.solenoidValve.initValve( 25 )
+def initInput( channel ):
+    GPIO.setup( channel, GPIO,IN )
+
+def initOutput( channel ):
+    GPIO.setup( channel, GPIO.OUT )
+
+
+for i in gpios:
+    try:
+        initInput( i )
+    except:
+        pass
+    try:
+        initOutput( i )
+    except:
+        pass
 
 GPIO.cleanup()
